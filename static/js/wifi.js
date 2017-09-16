741c9b16459d48fb6a2fb3534e616d9b4e4dcc56
Guru Prasad Srinivasa <gurupras@buffalo.edu>
2017-09-16 07:48:25 -0400

    Fixed a race condition where socket would be used before ready

--- static/js/wifi.js
+++ static/js/wifi.js
@@ -5,56 +5,56 @@ window.onload = function() {
 
 
 	socket = SimpleWebSocket.connect();
-
-	// Check connectivity status
-	// If we're running hotspot, then we show the wi-fi page
-	socket.on('wifi-status', function(msg) {
-		var json = JSON.parse(msg);
-		if(json.hotspot) {
-		}
-	});
-
-	socket.on('wifi-scan-results', function(msg) {
-		console.log("Received scan results: " + JSON.stringify(msg))
-		function __addScanResult(entry) {
-			var html = '<li class="collection-item" data-interface="' + entry.interface + '" data-data-ssid="' + entry.SSID + '">' + entry.SSID + '</li>';
-			var el = $($.parseHTML(html));
-			el.on('click', function(e) {
-				$('#wifi-ssid').val(entry.SSID);
-			});
-			$('#wifi-scan-results').append(el);
-		}
-
-		$('#wifi-scan-results').empty();
-
-		var json = JSON.parse(msg);
-		for(var i = 0; i < json.length; i++) {
-			var entry = json[i];
-			for(var j = 0; j < entry.scanResults.length; j++) {
-				entry.scanResults[j].interface = entry.interface;
-				__addScanResult(entry.scanResults[j]);
+	socket.onopen = function() {
+		// Check connectivity status
+		// If we're running hotspot, then we show the wi-fi page
+		socket.on('wifi-status', function(msg) {
+			var json = JSON.parse(msg);
+			if(json.hotspot) {
 			}
-		}
-		$('#wifi-scan-trigger').toggleClass("disabled", false);
-	});
+		});
 
-	$('#wifi-scan-trigger').on('click', function() {
-		$(this).toggleClass("disabled");
-		socket.emit('wifi-scan', "{}")
-	});
+		socket.on('wifi-scan-results', function(msg) {
+			console.log("Received scan results: " + JSON.stringify(msg))
+			function __addScanResult(entry) {
+				var html = '<li class="collection-item" data-interface="' + entry.interface + '" data-data-ssid="' + entry.SSID + '">' + entry.SSID + '</li>';
+				var el = $($.parseHTML(html));
+				el.on('click', function(e) {
+					$('#wifi-ssid').val(entry.SSID);
+				});
+				$('#wifi-scan-results').append(el);
+			}
+
+			$('#wifi-scan-results').empty();
+
+			var json = JSON.parse(msg);
+			for(var i = 0; i < json.length; i++) {
+				var entry = json[i];
+				for(var j = 0; j < entry.scanResults.length; j++) {
+					entry.scanResults[j].interface = entry.interface;
+					__addScanResult(entry.scanResults[j]);
+				}
+			}
+			$('#wifi-scan-trigger').toggleClass("disabled", false);
+		});
+
+		$('#wifi-scan-trigger').on('click', function() {
+			$(this).toggleClass("disabled");
+			socket.emit('wifi-scan', "{}")
+		});
 
 
-	$('#wifi-connect').on('click', function() {
-		var payload = {};
-		payload.SSID = $('#wifi-ssid').val();
-		payload.password = $('#wifi-password').val();
+		$('#wifi-connect').on('click', function() {
+			var payload = {};
+			payload.SSID = $('#wifi-ssid').val();
+			payload.password = $('#wifi-password').val();
 
-		console.log("Issuing wifi-connect event")
-		socket.emit('wifi-connect', JSON.stringify(payload));
-	});
+			console.log("Issuing wifi-connect event")
+			socket.emit('wifi-connect', JSON.stringify(payload));
+		});
 
-	socket.emit('wifi-scan');
-
+		socket.emit('wifi-scan');
+	}
 }
 
 
